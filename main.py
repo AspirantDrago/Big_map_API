cf141f48159b54a674aaf6e29f522809eb8692e1
Aleksandr <sanekclubstr@mail.ru>
2023-02-10 18:41:25 +0300

    Большая задача по Maps API. Часть №1 Оптимизировано сохранение изоражения в памяти

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
 import sys
-
 import requests
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
@@ -18,45 +16,46 @@ if hasattr(Qt, 'AA_UseHighDpiPixmaps'):
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-        self.getImage()
         self.initUI()
+        self.getImage()
 
     def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
-        response = requests.get(map_request)
+        url = "http://static-maps.yandex.ru/1.x/"
+        params = {
+            'll': '37.530887,55.703118',
+            'spn': '0.002,0.002',
+            'l': 'map'
+        }
+        response = requests.get(url, params=params)
 
         if not response:
             print("Ошибка выполнения запроса:")
-            print(map_request)
+            print(response.url)
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
 
-        # Запишем полученное изображение в файл.
-        self.map_file = "map.png"
-        with open(self.map_file, "wb") as file:
-            file.write(response.content)
+        self.pixmap.loadFromData(response.content, "PNG")
+        self.image.clear()
+        self.image.setPixmap(self.pixmap)
 
     def initUI(self):
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
-
-        ## Изображение
-        self.pixmap = QPixmap(self.map_file)
+        self.pixmap = QPixmap()
         self.image = QLabel(self)
         self.image.move(0, 0)
-        self.image.resize(600, 450)
+        self.image.resize(*SCREEN_SIZE)
         self.image.setPixmap(self.pixmap)
 
-    def closeEvent(self, event):
-        """При закрытии формы подчищаем за собой"""
-        os.remove(self.map_file)
 
 sys._excepthook = sys.excepthook
+
 
 def exception_hook(exctype, value, traceback):
     print(exctype, value, traceback)
     sys._excepthook(exctype, value, traceback)
     sys.exit(1)
+
 
 sys.excepthook = exception_hook
 
